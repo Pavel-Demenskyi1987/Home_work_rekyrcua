55be693a24cb7f32055a0d444ebb31bcc0928ffa
Pavel <shadow-hunter2000@yandex.ru>
2023-12-10 20:14:12 +0800

    Рекурсия

---
+++ Program1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Program
+{
+    public static void Main()
+    {
+        Console.Write("Введите число M = ");
+        int M = int.Parse(Console.ReadLine()!);
+        Console.WriteLine("Введите число  на котором надо остановить функцию: ");
+        int N = int.Parse(Console.ReadLine()!);
+    
+
+        if (M <= N)
+        {
+            Console.WriteLine($"Натуральные числа в промежутке от {M} до {N}:");
+            GetRec(M,N);
+        }
+        else
+        {
+            Console.WriteLine("Ошибка: M должно быть меньше или равно N.");
+        }
+    }
+
+    public static void GetRec(int M, int N)
+    {
+        if(M <= N)
+        {
+            Console.WriteLine(M);
+            GetRec(M + 1,N);
+        }
+        
+    }
+}
---
+++ Program3.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    public static void Main()
+    {
+        int[] array =  new int[]{9 ,5 ,4 ,7 ,2};
+        GetArray(array,  array.Length- 1);
+
+        Console.ReadLine(); 
+    }
+    
+
+    public static void GetArray(int[] arr, int index)
+    {
+        if (index < 0)
+            return;
+
+        Console.WriteLine(arr[index]);
+        GetArray(arr, index - 1);
+    }
+}
